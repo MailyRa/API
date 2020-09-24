4860e540ca4ec92f08ad294f4e5aeed63ad38981
Maily Ramos <maily.ramos1993@gmail.com>
2020-09-24 05:57:23 +0000

    class rectangle completed

--- env/api.py
+++ env/api.py
@@ -2,19 +2,48 @@ class Rectangle():
 
     def __init__(self, start_x, start_y, end_x, end_y, fill_char):
         # create a rectangle and return it
-        pass
+        self.start_x = start_x
+        self.start_y = start_y
+        self.end_x = end_x
+        self.end_y = end_y
+        self.fill_char = fill_char
+
+
+    def __repr__(self):
+
+        return f'<start_x="{self.start_x}" start_y="{self.start_y} end_x="{self.end_x} end_y="{self.end_y}" fill_char="{self.fill_char}">'
+
+
 
     def change_fill_char(self, new_fill_char):
         # update fill char
-        pass
+        self.fill_char = new_fill_char
     
     def translate(self, axis, num):
-        pass
+        #move left, right, up, or down
+        # x = moves left/right
+        # y = move up/down
+
+        if axis == 'x':
+            self.start_x += num
+            self.end_x += num
+
+        elif axis == 'y':
+            self.start_y += num
+            self.end_y += num
+        else:
+            ('Invalid, try again')
 
 class API():
 
-    def __init__(self):
-        pass
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+
+    def __repr__(self):
+
+        return f'<Length="{self.length}" Width="{self.width}">'
 
     def render_canvas(self):
         # Print the canvas and any shapes to standard output
